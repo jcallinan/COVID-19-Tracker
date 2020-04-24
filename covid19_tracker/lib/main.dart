4c805b8502bf0cfff192fc877d44b3a094b92ea8
Jeremy Callinan <jeremy.callinan@gmail.com>
2020-04-24 15:28:16 -0400

    Model updates

--- covid19_tracker/lib/main.dart
+++ covid19_tracker/lib/main.dart
@@ -1,6 +1,44 @@
 // home_material.dart
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'models/user.dart';
+import 'package:http/http.dart' as http;
+Future<User> sendEvent(String firstName, String lastName, String temperature, String dateTimeTaken,String notes) async {
+  final http.Response response = await http.post(
+    'https://argslsdev.eastus.cloudapp.azure.com:16500//EventService.asmx/SaveEvent',
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'firstName': firstName,
+      'lastName': lastName,
+      'temperature': temperature,
+      'dateTimeTaken': dateTimeTaken,
+      'notes': notes
+    }),
+  );
+
+  if (response.statusCode == 201) {
+    return User.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to create album.');
+  }
+}
+
+class Album {
+  final int id;
+  final String title;
+
+  Album({this.id, this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
 void main() => runApp(MaterialApp(
 title: 'Flutter Database Example',
 home: MyApp(), ));
@@ -10,6 +48,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _COVIDCollectDataState extends State {
+
+  Future<User> futureUser;
   final _formKey = GlobalKey();
   final _user = User();
   @override
@@ -31,6 +71,8 @@ class _COVIDCollectDataState extends State {
                             validator: (value) {
                               if (value.isEmpty) {
                                 return 'Please enter your first name';
+                              } else {
+                                return '';
                               }
                             },
                             onSaved: (val) =>
@@ -43,6 +85,9 @@ class _COVIDCollectDataState extends State {
                                 if (value.isEmpty) {
                                   return 'Please enter your last name.';
                                 }
+                                else {
+                                  return '';
+                                }
                               },
                               onSaved: (val) =>
                                   setState(() => _user.lastName = val)),
@@ -52,6 +97,8 @@ class _COVIDCollectDataState extends State {
                               validator: (value) {
                                 if (value.isEmpty) {
                                   return 'Please enter the temperature taken.';
+                                } else {
+                                  return '';
                                 }
                               },
                               onSaved: (val) =>
@@ -76,14 +123,43 @@ class _COVIDCollectDataState extends State {
                               child: RaisedButton(
                                   onPressed: () {
                                     final form = _formKey.currentState;
+                                    setState(() {
+
+                                      futureUser = sendEvent(_user.firstName,_user.lastName,_user.dateTimeTaken,_user.temperature.toString(),_user.notes);
+                                      _user.firstName = "";
+                                      _user.lastName = "";
+                                      _user.temperature = 0;
+                                      _user.dateTimeTaken = "";
+                                      _user.notes = "";
+                                    });
                                    /* if (form.validate()) {
                                       form.save();
                                       _user.save();
                                       _showDialog(context);
                                     }*/
                                   },
-                                  child: Text('Save'))),
-                        ])))));
+                                  child: Text('Save')),
+
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 16.0, horizontal: 16.0),
+                            child: RaisedButton(
+                                onPressed: () {
+                               _user.firstName = "";
+                               _user.lastName = "";
+                               _user.temperature = 0;
+                               _user.dateTimeTaken = "";
+                               _user.notes = "";
+
+                                },
+                                child: Text('Reset')),
+
+                          ),
+                        ]))
+            )
+        )
+    );
   }
   _showDialog(BuildContext context) {
     Scaffold.of(context)
--- covid19_tracker/lib/models/user.dart
+++ covid19_tracker/lib/models/user.dart
@@ -5,7 +5,18 @@ class User {
   double temperature = 0.0;
   String dateTimeTaken;
   String notes;
+  User({this.firstName, this.lastName, this.temperature, this.dateTimeTaken, this.notes});
+
   save() {
     print('saving user');
+  }
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      temperature: json['temperature'],
+      dateTimeTaken: json['dateTimeTaken'],
+      notes: json['notes'],
+    );
   }
 }
