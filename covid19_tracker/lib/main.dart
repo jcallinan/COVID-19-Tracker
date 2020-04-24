a186ee9fc85f589ef8f45284a06c1c2071162001
Jeremy Callinan <jeremy.callinan@gmail.com>
2020-04-24 15:36:31 -0400

    UI Changes

--- covid19_tracker/lib/main.dart
+++ covid19_tracker/lib/main.dart
@@ -21,26 +21,14 @@ Future<User> sendEvent(String firstName, String lastName, String temperature, St
   if (response.statusCode == 201) {
     return User.fromJson(json.decode(response.body));
   } else {
-    throw Exception('Failed to create album.');
+    throw Exception('Failed to save event.');
   }
 }
 
-class Album {
-  final int id;
-  final String title;
 
-  Album({this.id, this.title});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      id: json['id'],
-      title: json['title'],
-    );
-  }
-}
 
 void main() => runApp(MaterialApp(
-title: 'Flutter Database Example',
+title: 'COVID-19 Tracker',
 home: MyApp(), ));
 class MyApp extends StatefulWidget {
   @override
